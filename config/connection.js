f349277fd7b2fefa389bc15d8e4cfbc9aa5e9216
fab1214 <88295561+fab1214@users.noreply.github.com>
2021-12-17 23:00:08 -0500

    try to fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,21 +9,10 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     });
   }
 
 module.exports = sequelize;
-
-// // create connection to our db
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     });
-
-// module.exports = sequelize;
